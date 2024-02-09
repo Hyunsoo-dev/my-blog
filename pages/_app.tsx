160defda60e7d699a4bbb7eec4b7db7242499c56
Hyunsoo-dev <jing07161@gmail.com>
2024-02-10 00:00:30 +0900

    fix: 폰트 스타일 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import '@/styles/globals.css';
-import localFont from 'next/font/local';
 import type { AppProps } from 'next/app';
+// import localFont from 'next/font/local';
+import { Inter } from 'next/font/google';
+import Layout from '../component/layout/layout';
 
-const bmJua = localFont({
-	src: [
-		{
-			path: '../public/font/BMJUA.otf',
-			weight: 'normal',
-		},
-	],
-});
+const inter = Inter({ subsets: ['latin'] });
+// const bmJua = localFont({
+// 	src: [
+// 		{
+// 			path: '../public/font/BMJUA.otf',
+// 			weight: 'normal',
+// 		},
+// 	],
+// });
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<main className={`${bmJua.className}`}>
-			<Component {...pageProps} />
+		<main className={`${inter.className}`}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</main>
 	);
 }
