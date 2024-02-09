2abbdb376ca0fbaf22cc2065a07710696cb1af15
Hyunsoo-dev <jing07161@gmail.com>
2024-02-09 22:32:14 +0900

    feature: 폰트 설정 및 home, blog 페이지 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import localFont from 'next/font/local';
+import type { AppProps } from 'next/app';
 
+const bmJua = localFont({
+	src: [
+		{
+			path: '../public/font/BMJUA.otf',
+			weight: 'normal',
+		},
+	],
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<main className={`${bmJua.className}`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
